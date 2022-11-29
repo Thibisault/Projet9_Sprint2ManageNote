8887c1abaae416202d2c343ae392749e24e74809
Thibisault <72867272+Thibisault@users.noreply.github.com>
2022-11-29 07:32:48 +0100

    first commit

---
+++ src/main/java/com/example/Sprint2ManageNote/Sprint2ManageNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sprint2ManageNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint2ManageNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint2ManageNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sprint2ManageNote/Sprint2ManageNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sprint2ManageNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint2ManageNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
